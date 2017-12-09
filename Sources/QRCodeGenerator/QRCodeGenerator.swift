db645c3d13a4cbe6c18187ef0325d714befd8751
Fran Fernández <fjfdeztoro@gmail.com>
2017-12-09 18:03:54 +0100

    Change QRCodeGenerator class visibility to public.

--- Sources/QRCodeGenerator/QRCodeGenerator.swift
+++ Sources/QRCodeGenerator/QRCodeGenerator.swift
@@ -1,7 +1,7 @@
 import QRCodeGenerator_Wrapper
 
-class QRCodeGenerator {
-    static func getQRCodeAsSVG(_ text: String, withTolerance tolerance: ErrorTolerance) -> String {
+public class QRCodeGenerator {
+    public static func getQRCodeAsSVG(_ text: String, withTolerance tolerance: ErrorTolerance) -> String {
         return QrCodeWrapper.toSVGXML(text, with: tolerance)
     }
 }
