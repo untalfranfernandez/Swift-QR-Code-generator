6c80716c462d6d8b75e731bdbd520e28e0a49e20
Fran Fernandez <fran@gokarumi.com>
2017-12-11 22:40:39 +0100

    First approach, compiling in MacOS.

--- Package.swift
+++ Package.swift
@@ -15,9 +15,15 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(name: "QRCodeGenerator", dependencies: ["QRCodeGenerator-Wrapper"]),
-        .target(name: "QRCodeGenerator-Wrapper", dependencies: ["QRCodeGenerator-CPP"]),
         .target(name: "QRCodeGenerator-CPP", dependencies: []),
         .testTarget(name: "QRCodeGeneratorTests", dependencies: ["QRCodeGenerator", "QRCodeGenerator-Wrapper"]),
     ],
     cxxLanguageStandard: .cxx11
 )
+
+#if os(Linux)
+    package.dependencies.append(.package(url:"https://github.com/fjfdeztoro/SwiftNUStep", from: "0.0.1"))
+    package.targets.append(.target(name: "QRCodeGenerator-Wrapper", dependencies: ["QRCodeGenerator-CPP", "GNUStep"]))
+#else
+    package.targets.append(.target(name: "QRCodeGenerator-Wrapper", dependencies: ["QRCodeGenerator-CPP"]))
+#endif
